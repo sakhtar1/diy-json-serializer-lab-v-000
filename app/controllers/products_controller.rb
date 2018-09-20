85b7015c6da24d7f35d50d1f882036b8b710954b
Sharmina Akhtar <sakht318@gmail.com>
2018-09-20 02:33:15 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,22 +13,22 @@ class ProductsController < ApplicationController
     render plain: product.description
   end
 
-  def data
-    product = Product.find(params[:id])
-    render json: ProductSerializer.serialize(product)
-  end
-
   def new
     @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
   end
 
   def show
     @product = Product.find(params[:id])
   end
 
-  def create
-    Product.create(product_params)
-    redirect_to products_path
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
   end
 
   private
