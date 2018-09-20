dad9a14f3111e8419a1149e908709f85916e8088
Sharmina Akhtar <sakht318@gmail.com>
2018-09-20 02:47:10 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,18 +1,13 @@
-def ProductSerializer
+class ProductSerializer
   def self.serialize(product)
+    serialized_product = '{'
 
-      # start with the open brace to create a valid JSON object
-      serialized_product = '{'
+    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"description": "' + product.description + '", '
+    serialized_product += '"inventory": ' + product.inventory.to_s + ', '
+    serialized_product += '"price": "' + product.price.to_s + '"'
 
-      serialized_product += '"id": ' + product.id.to_s + ', '
-      serialized_product += '"name": "' + product.name + '", '
-      serialized_product += '"description": "' + product.description + '", '
-      serialized_product += '"price": "' + product.price.to_s + '", '
-      serialized_product += '"inventory": "' + product.inventory.to_s + '", '
-
-
-
-      # and end with the close brace
-      serialized_product += '}'
-    end
+    serialized_product += '}'
+  end
 end
