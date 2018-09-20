9bf12ccd01a187bc68f1e20969c208f08ecc7c8e
Sharmina Akhtar <sakht318@gmail.com>
2018-09-20 02:23:06 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,17 +4,17 @@ class ProductsController < ApplicationController
   end
 
   def inventory
-    product = Product.find(params[:id])
+    @product = Product.find(params[:id])
     render plain: product.inventory > 0 ? true : false
   end
 
   def description
-    product = Product.find(params[:id])
+    @product = Product.find(params[:id])
     render plain: product.description
   end
 
   def data
-    product = Product.find(params[:id])
+    @product = Product.find(params[:id])
     render plain: product.description
   end
 
