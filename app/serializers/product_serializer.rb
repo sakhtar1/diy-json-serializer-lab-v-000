98626b0646c443d4b551a7470d2d743837e2def8
Sharmina Akhtar <sakht318@gmail.com>
2018-09-20 02:01:00 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -6,9 +6,9 @@ def ProductSerializer
 
       serialized_product += '"id": ' + product.id.to_s + ', '
       serialized_product += '"name": "' + product.name + '", '
+      serialized_product += '"description": "' + product.description + '", '
       serialized_product += '"price": "' + product.price + '", '
       serialized_product += '"inventory": "' + product.inventory + '", '
-      serialized_product += '"description": "' + product.description + '", '
 
 
 
